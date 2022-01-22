c0150e6c69b970c7e40ab189f952ad5848f6da61
joe <joewalsh@vistulaofindiana.com>
2022-01-22 00:52:26 -0500

    made MonthSetup sheet to intake

--- google_api_calls_abstract.py
+++ google_api_calls_abstract.py
@@ -1,97 +1,89 @@
 
-def simple_batch_update(service, sheet_id, wrange, data, dim):
-    print(f"Updating {sheet_id} with batch call to {wrange}...")
-    # sheet = service.spreadsheets()
-    #range_ = wrange
-    body_request = {
-                    'value_input_option': 'RAW',
-                    'data': [
-                            {'range': wrange,
-                            'majorDimension': dim, #'row' or 'column'
-                             'values': [data]
-                             }
-                    ],
-                    }
+class GoogleApiCalls:
 
-    request = service.spreadsheets().values().batchUpdate(spreadsheetId=sheet_id, body=body_request)
-    response = request.execute()
-    print(response)
+    def simple_batch_update(self, service, sheet_id, wrange, data, dim):
+        print(f"Updating {sheet_id} with batch call to {wrange}...")
+        body_request = {
+                        'value_input_option': 'RAW',
+                        'data': [
+                                {'range': wrange,
+                                'majorDimension': dim, #'ROW' or 'COLUMN'
+                                'values': [data]
+                                }
+                        ],
+                        }
 
-def update(service, sheet_choice, data, write_range):
-    sheet = service.spreadsheets()
-    print(sheet_choice)
-    spreadsheet_id = sheet_choice
+        request = service.spreadsheets().values().batchUpdate(spreadsheetId=sheet_id, body=body_request)
+        response = request.execute()
+        print(response)
 
-    range_ = write_range  # TODO: Update placeholder value.
+    def update(self, service, sheet_choice, data, write_range):
+        sheet = service.spreadsheets()
+        print(sheet_choice)
+        spreadsheet_id = sheet_choice
 
-    value_input_option = 'RAW'  #
+        range_ = write_range  # TODO: Update placeholder value.
 
-    value_range_body = {"range": write_range,
-                        "majorDimension": "COLUMNS",
-                        "values": [data]
-    }
+        value_input_option = 'RAW'  #
 
-    request = service.spreadsheets().values().update(spreadsheetId=spreadsheet_id, range=range_, valueInputOption=value_input_option, body=value_range_body)
-    response = request.execute()
+        value_range_body = {"range": write_range,
+                            "majorDimension": "COLUMNS",
+                            "values": [data]
+        }
 
-    print(response)
+        request = service.spreadsheets().values().update(spreadsheetId=spreadsheet_id, range=range_, valueInputOption=value_input_option, body=value_range_body)
+        response = request.execute()
 
-def write_formula_column(service, sheet_choice, data, write_range):
-    print(f"Writing {data} to {write_range}  . . .")
-    sheet = service.spreadsheets()
-    spreadsheet_id = sheet_choice
-    range_ = write_range  # TODO: Update placeholder value.
-    # How the input data should be interpreted.
-    value_input_option = 'USER_ENTERED'
-    value_range_body = {
-                        "values": [data]
-    }
 
-    request = service.spreadsheets().values().update(spreadsheetId=spreadsheet_id, range=range_, valueInputOption=value_input_option, body=value_range_body)
-    response = request.execute()
+    def write_formula_column(self, service, sheet_choice, data, write_range):
+        print(f"Writing {data} to {write_range}  . . .")
+        sheet = service.spreadsheets()
+        spreadsheet_id = sheet_choice
+        range_ = write_range  # TODO: Update placeholder value.
+        # How the input data should be interpreted.
+        value_input_option = 'USER_ENTERED'
+        value_range_body = {
+                            "values": [data]
+        }
 
-def format_row(service, sheet_choice, write_range, r_or_c, name_list): # and writing strings to ranges passed
-    print(f"Formatting {r_or_c} . . .")
-    sheet = service.spreadsheets()
-    spreadsheet_id = sheet_choice
+        request = service.spreadsheets().values().update(spreadsheetId=spreadsheet_id, range=range_, valueInputOption=value_input_option, body=value_range_body)
+        response = request.execute()
 
-    range_ = write_range  # TODO: Update placeholder value.
+    def format_row(self, service, sheet_choice, write_range, r_or_c, name_list): # and writing strings to ranges passed
+        print(f"Formatting {r_or_c} . . .")
+        sheet = service.spreadsheets()
+        spreadsheet_id = sheet_choice
 
-    # How the input data should be interpreted.
-    value_input_option = 'USER_ENTERED'  #
+        range_ = write_range  # TODO: Update placeholder value.
 
-    value_range_body = {"range": write_range,
-                        "majorDimension": r_or_c,
-                        "values": [name_list]
-        # TODO: Add desired entries to the request body. All existing entries
-        # will be replaced.
-    }
+        value_input_option = 'USER_ENTERED'  #
 
-    request = service.spreadsheets().values().update(spreadsheetId=spreadsheet_id, range=range_, valueInputOption=value_input_option, body=value_range_body)
-    response = request.execute()
+        value_range_body = {"range": write_range,
+                            "majorDimension": r_or_c,
+                            "values": [name_list]
+        }
 
-def clear_sheet(service, sheet_choice, clear_range):
-    service = service
-    spreadsheet_id = sheet_choice
+        request = service.spreadsheets().values().update(spreadsheetId=spreadsheet_id, range=range_, valueInputOption=value_input_option, body=value_range_body)
+        response = request.execute()
 
-    batch_clear_values_request_body = {
-                                    'ranges': [clear_range],
+    def clear_sheet(self, service, sheet_choice, clear_range):
+        service = service
+        spreadsheet_id = sheet_choice
 
-                                    }
-        # 'ranges': [],
+        batch_clear_values_request_body = {
+                                        'ranges': [clear_range],
 
-        # TODO: Add desired entries to the request body.
+                                        }
+        request = service.spreadsheets().values().batchClear(spreadsheetId=spreadsheet_id, body=batch_clear_values_request_body)
+        response = request.execute()
 
-    request = service.spreadsheets().values().batchClear(spreadsheetId=spreadsheet_id, body=batch_clear_values_request_body)
-    response = request.execute()
-
-def broad_get(service, spreadsheet_id, range):
-    sheet = service.spreadsheets()
-    result = sheet.values().get(spreadsheetId=spreadsheet_id,
-                                range=range).execute()
-    values = []
-    values_ = result.get('values', [])
-    for value in values_:
-        values.append(value)
-    return values
+    def broad_get(self, service, spreadsheet_id, range):
+        sheet = service.spreadsheets()
+        result = sheet.values().get(spreadsheetId=spreadsheet_id,
+                                    range=range).execute()
+        values = []
+        values_ = result.get('values', [])
+        for value in values_:
+            values.append(value)
+        return values
 
--- setup_month.py
+++ setup_month.py
@@ -1,6 +1,7 @@
 from config import Config, my_scopes
 from auth_work import oauth
 from utils import Utils
+from google_api_calls_abstract import GoogleApiCalls
 import pathlib
 import pandas as pd
 
@@ -11,7 +12,14 @@ class MonthSheet:
     G_SUM_KRENT = ["=sum(E2:E68)"]
     G_SUM_ACTSUBSIDY = ["=sum(F2:F68)"]
     G_SUM_ACTRENT = ["=sum(H2:H68)"]
-  
+    ui_sheet = 'intake'
+    range1 = '1'
+    range2 = '100'
+    wrange_unit1 = f'{ui_sheet}!A{range1}:A{range2}'
+    wrange_t_name1 = f'{ui_sheet}!B{range1}:B{range2}'
+    wrange_k_rent1 = f'{ui_sheet}!c{range1}:c{range2}'
+    wrange_subsidy1 = f'{ui_sheet}!d{range1}:d{range2}'
+    wrange_t_rent1 = f'{ui_sheet}!e{range1}:e{range2}'
 
     def __init__(self, full_sheet, path):
         self.test_message = 'hi'
@@ -20,6 +28,16 @@ class MonthSheet:
         self.user_choice = None
         self.file_input_path = path
         self.user_text = f'Options:\n PRESS 1 to show current sheets in RENT SHEETS \n PRESS 2 TO VIEW ITEMS IN {self.file_input_path} \n PRESS 3 for MONTHLY FORMATTING, PART ONE (that is, update intake sheet in {self.file_input_path} (xlsx) \n PRESS 4 for MONTHLY FORMATTING, PART TWO: format rent roll & subsidy by month and sheet\n >>>'
+        self.wrange_unit = self.wrange_unit1
+        self.wrange_t_name = self.wrange_t_name1
+        self.wrange_k_rent = self.wrange_k_rent1 
+        self.wrange_subsidy = self.wrange_subsidy1
+        self.wrange_t_rent = self.wrange_t_rent1 
+        self.t_name = []
+        self.unit = [] 
+        self.k_rent = [] 
+        self.subsidy = [] 
+        self.t_rent = []
 
     def control(self):
         if self.user_choice == 1:
@@ -45,16 +63,37 @@ class MonthSheet:
         for item in current_items:
             print(item.name)
 
+    def read_excel(self, verbose=False):
+        df = pd.read_excel(self.file_input_path, header=16)
+        if verbose: 
+            pd.set_option('display.max_columns', None)
+            print(df.head(100))
+        t_name = df['Name'].tolist()
+        unit = df['Unit'].tolist()
+        k_rent = df['Lease Rent'].tolist()
+        t_rent = df['Actual Rent Charge'].tolist()
+        subsidy = df['Actual Subsidy Charge'].tolist()
+
+        return self.fix_data(t_name), self.fix_data(unit), self.fix_data(k_rent), self.fix_data(subsidy), self.fix_data(t_rent)
+
+    def fix_data(self, item):
+        item.pop()
+        return item
+
+    def write_to_rs(self):
+        gc = GoogleApiCalls()
+        gc.simple_batch_update(self.service, self.full_sheet, self.wrange_unit, self.unit, 'COLUMNS')
+        gc.simple_batch_update(self.service, self.full_sheet, self.wrange_t_name, self.t_name, 'COLUMNS')
+        gc.simple_batch_update(self.service, self.full_sheet, self.wrange_k_rent, self.k_rent, 'COLUMNS')
+        gc.simple_batch_update(self.service, self.full_sheet, self.wrange_subsidy, self.subsidy, 'COLUMNS')
+        gc.simple_batch_update(self.service, self.full_sheet, self.wrange_t_rent, self.t_rent, 'COLUMNS')
+
     def push_to_intake(self):
         print('pushing selected excel to intake', '| Path:', self.file_input_path)
-        path = Utils.sheet_finder(path=self.file_input_path, function='month setup')
-        df = pd.read_excel(path, header=16)
-        print(df.head(100))
+        self.file_input_path = Utils.sheet_finder(path=self.file_input_path, function='month setup')
+        self.t_name, self.unit, self.k_rent, self.subsidy, self.t_rent = self.read_excel(verbose=False)
+        self.write_to_rs()
 
-        # select xlrd(xls) or openpyxl(xlsx)
-
-        # sheet = Utils.load_activate_workbook(path)
-        # print(sheet)
 
     def show_utils(self):
         for k, item in Utils.__dict__.items():
@@ -65,40 +104,7 @@ ms = MonthSheet(full_sheet=Config.TEST_RS, path=Config.RS_DL_FILE_PATH)
 ms.set_user_choice()
 ms.control()
 
-# I THINK PANDAS CAN JUST PICK BY FILE TYPE: SO I CHOOSE AND THEN PANDAS CAN GRAB IF XLS OR CSV OR XLSX
+# set up testing, sheet clearing
+# formatting of intake, what else can I do with this data, setup sqlite and
+    # rent potential
 # I HAVE A PROBLEM GETTING THE FILE INTO WSL FOLDER: i CAN DO IT WITH LINUX OR VSCODE, BUT NOT WINDOWS EXPLORER
-
-
-# def keetchen_saink(self, sheet): 
-#         """This is a custom job; tailored to the specific format of sheet for realpage deposits"""
-
-#         pprint("Using openpyxl to iterate through rows of excel sheet . . .")
-#         pprint("Generating lists of transactions et al from sheet . . . ")
-
-#         bde = []
-#         unit=[] #1
-#         name=[] #3
-#         date=[] #7
-#         pay=[] #13
-
-#         pay_float = []
-
-#         for value in sheet.iter_rows(min_row=11, min_col=1, max_col=14, values_only=True):
-
-#             if value[1] != None:
-#                 bde.append(value[1])
-#                 unit.append(value[0])
-#                 name.append(value[2])
-#                 date.append(value[5])
-#                 pay.append(value[13])
-
-#             else:
-#                 print(value)
-#                 print("******")
-
-#         print("******")
-#         pay_float = [float(item) for item in pay]
-#         dt_code = [item[0:2]for item in date] # makes date code
-#         print("******")
-
-#         return bde, unit, name, date, pay, pay_float, dt_code
