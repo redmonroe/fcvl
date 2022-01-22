eea0e6b1f643a080a218f463d39ef1218346875d
joe <joewalsh@vistulaofindiana.com>
2022-01-22 15:17:42 -0500

    cleanup

--- setup_month.py
+++ setup_month.py
@@ -145,11 +145,11 @@ class MonthSheet:
 
 
 ms = MonthSheet(full_sheet=Config.TEST_RS, path=Config.RS_DL_FILE_PATH)
-ms.delete_table()
+# ms.delete_table()
 # ms.export_to_sqlite()
 
-# ms.set_user_choice()
-# ms.control()
+ms.set_user_choice()
+ms.control()
 
 # the PRESS 1 IS READY GUARD IS FUCKING UP
 # set up testing, sheet clearing
