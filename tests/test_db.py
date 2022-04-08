3ece2c2e4b25f4f825d63a6637d9e831536ef3bf
joe <joewalsh@vistulaofindiana.com>
2022-04-08 19:10:27 -0400

    add db setup to test

--- tests/test_db.py
+++ tests/test_db.py
@@ -1,4 +1,7 @@
 import pytest
+from backend import db, Tenant
+
+create_tables_list = [Tenant]
 
 @pytest.mark.testing_db
 class TestDB:
@@ -7,4 +10,12 @@ class TestDB:
 
     def test_init(self):
         assert self.test_message == 'hi'
+
+    def test_db(self):
+        db.connect()
+        db.create_tables(create_tables_list)
+        assert db.database == '/home/joe/local_dev_projects/fcvl/sqlite/test_pw_db.db'
+        assert db.get_tables() == ['tenant']
+        assert db.get_columns(table='tenant')[0]._asdict() == {'name': 'id', 'data_type': 'INTEGER', 'null': False, 'primary_key': True, 'table': 'tenant', 'default': None}
         
+        breakpoint()
