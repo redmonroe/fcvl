ca77cf28b5f6653ccc445d27b38fd4d263e4c522
joe <joewalsh@vistulaofindiana.com>
2022-06-14 00:47:56 -0400

    make more funcs for updating Payment

--- manual_entry.py
+++ manual_entry.py
@@ -1,4 +1,5 @@
-from backend import db, BaseModel, PopulateTable, Payment
+from backend import db, PopulateTable, Payment
+from datetime import datetime as dt
 import sys
 
 # we can do a manual entry and then to persist it for testing we will
@@ -17,17 +18,23 @@ class ManualEntry:
         self.populate = PopulateTable()
 
     def main(self):
-        print('\n starting manual entry flow')
-        tables_list = self.what_tables_available()
-        selection = self.selection_ui(list1=tables_list)
-        years = self.what_years_available(selection=selection)
+        months = set([rec.date_posted for rec in Payment.select()])
+        year = list(months)[0].year
+        months = set([month.month for month in months])
+        selection = self.selection_ui(list1=months, header1=year, header2='months', header3=Payment)
+        rows = self.what_rows_payments(target=selection)
 
-        if len(years) > 1:
-            selection = self.selection_ui(list1=years)
-        else:
-            print(f'working with year {years[0]}')
+        selection = self.selection_ui(list1=rows, header1=year, header2='transactions', header3=Payment)
+
+        selected_item = self.find_by_id(selection=selection)
+        breakpoint()
+
+        modified_item = self.selection_ui(list1=selected_item, header1=year, header2='transactions', header3=Payment)
+        
             
-    def selection_ui(self, list1=None):
+    def selection_ui(self, list1=None, header1=None, header2=None, header3=None):
+        print(f'showing {header2} in {header1} for table {header3}')
+
         choices = []
         for count, item in enumerate(list1, 1):
             print(count, item)
@@ -37,11 +44,19 @@ class ManualEntry:
         choice = int(input('please enter a number from the above list: '))
         selection = [item for count, item in choices if count == choice][0]
         print(f'you chose {selection}')
-        return selection
-
+        return selection            
+    
+    def find_by_id(self, selection=None):
+        selected_record = Payment.get(Payment.id==selection[0])
+        return selected_record
+    
     def what_tables_available(self):
         tables_list = self.populate.return_tables_list()
         return tables_list
+
+    def what_rows_payments(self, target=None):
+        rows = [(row.id, row.tenant, row.amount, dt.strftime(row.date_posted, '%Y-%m-%b')) for row in Payment.select().where(Payment.date_code==target).namedtuples()]
+        return rows
 
     def what_years_available(self, selection=None):
         years = set([rec.date_posted for rec in selection.select()])
