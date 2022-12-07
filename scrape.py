2bd382e91d51e781b59f0fdafdda862c9f087878
joe <joewalsh@vistulaofindiana.com>
2022-12-07 02:18:38 -0500

    got one scrape to ffxking work

--- scrape.py
+++ scrape.py
@@ -5,13 +5,14 @@ class Scrape:
     def hello(self):
         print('hello from scrape')
 
-    def pw_context(self):
+    def pw_context(self, path):
         with sync_playwright() as playwright:
-            self.run_realpage_test(playwright)
+            self.run_realpage_test(playwright, path)
     
-    def run_realpage_test(self, playwright):
+    def run_realpage_test(self, playwright, path):
         browser = playwright.chromium.launch(headless=True)
         context = browser.new_context()
+        print(context)
 
         page = context.new_page()
 
@@ -21,44 +22,56 @@ class Scrape:
 
         page.get_by_placeholder("Username").click()
 
-        page.get_by_placeholder("Username").click()
-
         page.get_by_placeholder("Username").fill("JWalsh4")
 
-        page.get_by_placeholder("Username").press("Enter")
+        page.get_by_role("button").click()
         page.wait_for_url("https://www.realpage.com/login/identity/Account/Local")
 
+        page.get_by_placeholder("Password").click()
+        page.get_by_placeholder("Password").fill("Freedom7!")
         page.locator("button").click()
         page.wait_for_url("https://www.realpage.com/home/")
 
-        with page.expect_popup() as popup_info:
-            page.get_by_role("link", name="OneSite OneSite L&R, Budgeting, Payments, Screening, Purchasing, and Doc. Mgmt").click()
-        page1 = popup_info.value
-        page.wait_for_url("https://vistula.onesite.realpage.com/multishell_cb/shell.htm?c=101101000011000000001111001000000010000000000000000000000000000100000000000100000000000000,818328946,JWALSH4&u=221,1111110110000000010000010000&t=0")
-        breakpoint()
-
-        page2.goto("https://vistula.onesite.realpage.com/ui2/dashboards/#/")
+        #take screenshot with playwright    
 
         page.get_by_role("menuitem", name="Reports").get_by_role("link", name="Reports").click()
         page.wait_for_url("https://www.realpage.com/reporting/")
 
         page.goto("https://www.realpage.com/reporting/")
 
-        page.get_by_placeholder("Search...").click()
+        page.get_by_text("Bank Deposit Details (Excel)").click()
 
-        page.get_by_placeholder("Search...").fill("rent roll")
+        page.get_by_role("button", name="Select Property").click()
+        page.get_by_role("option", name="Falls Creek Village I").locator("label div").click()
+        page.get_by_role("option", name="Falls Creek Village I").press("Tab")
+        page.get_by_role("button", name="Current AR Period").press("Tab")
+        page.get_by_role("button", name="1").click()
+        page.get_by_role("textbox", name="Search").fill("all")
+        page.get_by_role("option", name="ALL").get_by_text("ALL").click()
 
-        page2.close()
+        page.screenshot(path="screenshot.png")
+        page.get_by_role("button", name="Generate").click()
 
-        page1.close()
+        page.locator("raul-grid-row[role=\"listitem\"]:has-text(\"Bank Deposit Details (Excel) Generate & Schedule Add to Group View Scheduled Vie\")").get_by_role("button").click()
+
+        page.get_by_role("button", name="View Completed").click()
+
+        page.get_by_role("button", name="Select Properties").click()
+
+        page.get_by_role("option", name="Falls Creek Village I").locator("label div").click()
+
+        page.locator("raul-grid-row[role=\"listitem\"]:has-text(\"Completed Date12/07/2022 01:50 AMPropertyFalls Creek Village IUser / Report Grou\")").get_by_role("button").click()
+
+        with page.expect_download() as download_info:
+            page.get_by_role("button", name="Download").click()
+        download = download_info.value
+        print(download)
+        download.save_as(path)
+        breakpoint()
 
         page.close()
+
 
         # ---------------------
         context.close()
         browser.close()
-
-
-
-# with sync_playwright() as playwright:
-#     run(playwright)
